837f71fd426ba41176a4a5e74f4d93f490215e85
George G. Vega Yon <g.vegayon@gmail.com>
2024-09-26 19:27:31 -0600

    Adding a python script (will it work?)

---
+++ post-artifact/src/get-comment.py
@@ -0,0 +1,33 @@
+import sys
+import json
+
+def main(json_comments):
+
+    # Open the JSON file and load its contents into a Python object
+    with open(json_comments, 'r') as file:
+        data = json.load(file)
+
+    # Now you can work with the 'data' object
+    for i in range(len(data['comments'])):
+        body = data['comments'][i]['body']
+        auth = data['comments'][i]['author']['login']
+        id   = data['comments'][i]['id']
+
+        # Regex match to the body of the comment looking
+        # for the expression "Thank you for your contribution"
+        # if found, print the author and the body of the comment
+        if "Your preview" in body and auth == 'github-actions':
+            print(id)
+            return 0
+    
+    print('null')
+    return 0
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python get-comment.py comments.json")
+    else:
+        main(sys.argv[1])
+        
+    
+    
